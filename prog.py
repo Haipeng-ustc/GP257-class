87b0da95a45ab1d53832c514ecee550e19f3feff
Bob Clapp <bob@sep.stanford.edu>
2023-01-10 14:48:14 -0800

    added prog.py

---
+++ prog.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+
+
+def printMany(word:str,n:int)
+  """ I print word n times"""
+  for i in n:
+    print(word)
+
+
+
+
+if __name __ == "__main__":
+  printMany("Hello world",10)
