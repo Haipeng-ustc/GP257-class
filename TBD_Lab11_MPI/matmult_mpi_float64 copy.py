3e665b3df8d42fba9a4eed59abd72bd1d79b3657
Haipeng-ustc <haipeng@stanford.edu>
2023-03-08 04:30:23 +0000

    GPU

--- TBD_Lab11_MPI/matmult_mpi_float64 copy.py
+++ TBD_Lab11_MPI/matmult_mpi_float64 copy.py
@@ -35,7 +35,7 @@ size = int(np.sqrt(worker_size))
 block_size = int(N // size)
 
 # Setup the data type
-dtype = np.float32
+dtype = np.float64
 
 if rank == 0:
     # Check that the number of processes is a perfect square
@@ -61,33 +61,26 @@ local_A = np.zeros((block_size, N), dtype=dtype)
 local_B = np.zeros((N, block_size), dtype=dtype)
 
 if rank == 0:
+    # Set random values for A and B
+    A = np.random.rand(N, N).astype(dtype)
+    B = np.random.rand(N, N).astype(dtype)
 
     # Initialize local blocks of A and B for sending to each process
     block_A = np.zeros((block_size, N), dtype=dtype)
     block_B = np.zeros((N, block_size), dtype=dtype)
 
-    # Set random values for A once per block on process 0 and send to each process
+    # Send blocks of A and B to each process
     for i in range(size):
-        block_A = np.random.rand(block_size, N).astype(dtype)
         for j in range(size):
             proc = i*size+j
-
-            # print("Sending blocks of A", i, j, "to process", proc)
+            print("Sending block", i, j, "to process", proc)
+            block_A = np.ascontiguousarray(A[i*block_size:(i+1)*block_size, :])
+            block_B = np.ascontiguousarray(B[:, j*block_size:(j+1)*block_size])
             if proc == 0:
                 local_A = block_A
-            else:
-                comm.Send(block_A, dest=proc, tag=0)
-
-    # Set random values for B once per block on process 0 and send to each process
-    for j in range(size):
-        block_B = np.random.rand(N, block_size).astype(dtype)
-        for i in range(size):
-            proc = i*size+j
-
-            # print("Sending blocks of B", i, j, "to process", proc)
-            if proc == 0:
                 local_B = block_B
             else:
+                comm.Send(block_A, dest=proc, tag=0)
                 comm.Send(block_B, dest=proc, tag=1)
 else:
     # Receive local blocks of A and B from process 0 and store in local_A and local_B
@@ -115,11 +108,11 @@ else:
                 C[i*block_size:(i+1)*block_size, 
                   j*block_size:(j+1)*block_size] = local_C
 
-# # Compare the MPI result with the serial result from np.matmul
-# if rank == 0:
-#     print("\nMPI result == Serial result: ", np.allclose(C, np.matmul(A, B)))
-#     assert np.allclose(C, np.matmul(A, B)), "MPI result != Serial result"
-#     print("\n")
+# Compare the MPI result with the serial result from np.matmul
+if rank == 0:
+    print("\nMPI result == Serial result: ", np.allclose(C, np.matmul(A, B)))
+    assert np.allclose(C, np.matmul(A, B)), "MPI result != Serial result"
+    print("\n")
     
 # Stop the MPI and clean up the MPI environment
 MPI.Finalize()
